58b74ea7ec0dbac6c716fa402f77c215973b7151
Javier Enrique Hern�ndez M�rquez <HM210444@alumno.udb.edu.sv>
2021-09-11 20:41:58 -0600

    subiendo prueba

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //For simple
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
